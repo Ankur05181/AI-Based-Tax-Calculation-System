57213449ece9ccae0e58f86d073ded37c6693ca5
Ankur Tiwary <ankurtiwary112@gmail.com>
2026-06-12 19:22:27 +0530

    improves code

--- Backend/taxsystem/src/main/java/com/tax/repository/UserRepository.java
+++ Backend/taxsystem/src/main/java/com/tax/repository/UserRepository.java
@@ -2,11 +2,9 @@ package com.tax.repository;
 
 import com.tax.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     // used in login
